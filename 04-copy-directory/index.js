52cff6278f8ace84a27eeb45e65331ffc966862b
KItsuneew <staskulesh.brest@gmail.com>
2024-01-20 22:27:09 +0300

    feat: complete 04-copy-directory

---
+++ 04-copy-directory/index.js
@@ -0,0 +1,23 @@
+const fs = require('node:fs');
+const path = require('node:path');
+
+const pathForFolderElement = path.join(__dirname, 'files');
+const pathForFolderCopy = path.join(__dirname, 'files-copy');
+
+async function copyFolder() {
+  await fs.promises.rm(pathForFolderCopy, { force: true, recursive: true });
+  await fs.promises.mkdir(pathForFolderCopy, { recursive: true });
+
+  const currentFiles = await fs.promises.readdir(
+    pathForFolderElement,
+    { withFileTypes: true },
+    (_, el) => {},
+  );
+
+  currentFiles.forEach((el) => {
+    const oldFile = path.join(pathForFolderElement, el.name);
+    const newFile = path.join(pathForFolderCopy, el.name);
+    fs.promises.copyFile(oldFile, newFile);
+  });
+}
+copyFolder();
